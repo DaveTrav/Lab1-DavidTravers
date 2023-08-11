ba98e5b59befc6006e84544caab840770b20fa4b
DaveTrav <davetrav@hotmail.com>
2023-08-11 16:43:48 +0000

    Lab1-Q4
    Signed-off-by: DaveTrav <davetrav@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 //The reason using a flag variable in the interrupt function and then resetting it in the 'while' loop may not work as expected is due to potentian race conditions
 //If the while loop reads the flag at the same time it's being modified in the interrupt function, it can lead to unexpected behavior.
 //to avoid this issue you can disable interrupts while modifying the flag and while checking it in the 'while' loop.
-
+//an improved solution is to avoid directly manipulating the flag using interrupts, you can use the 'EventQueue' class provided by the mbed os. This allows you to queue events to be executed in a separate context, avoiding race conditions
 #include "mbed.h"
 
 
