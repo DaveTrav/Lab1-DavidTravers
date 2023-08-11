c00b5f4b39ba4033e394b7b6412f3b54a1abfeaf
DaveTrav <davetrav@hotmail.com>
2023-08-11 16:39:23 +0000

    Lab1-Q3
    Signed-off-by: DaveTrav <davetrav@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 //because of incorrect addresses and printf
 //The reason using a flag variable in the interrupt function and then resetting it in the 'while' loop may not work as expected is due to potentian race conditions
 //If the while loop reads the flag at the same time it's being modified in the interrupt function, it can lead to unexpected behavior.
+//to avoid this issue you can disable interrupts while modifying the flag and while checking it in the 'while' loop.
 
 #include "mbed.h"
 
