8a6b3ed7e7c052dec9199e5215cbfde6da3f2689
DaveTrav <davetrav@hotmail.com>
2023-08-11 16:34:59 +0000

    Lab1-Q2
    Signed-off-by: DaveTrav <davetrav@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
  */
 //mbed error message; mutex 0x20000380, not allowed in ISR context
 //because of incorrect addresses and printf
+//The reason using a flag variable in the interrupt function and then resetting it in the 'while' loop may not work as expected is due to potentian race conditions
+//If the while loop reads the flag at the same time it's being modified in the interrupt function, it can lead to unexpected behavior.
+
 #include "mbed.h"
 
 
